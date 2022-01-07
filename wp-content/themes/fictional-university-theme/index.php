71b142e7a0c7763a1ae2c68bef0af67bc3ba9d9e
jtdeleon <jondeleon856@gmail.com>
2022-01-07 08:16:54 -0500

    play with theme functions

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,3 +1,11 @@
-This is the amazing custom theme! 
+<?php  
+    function greet($name, $color) {
+        echo "<p>Hi, my name is $name and my favorite color is $color</p>";
+    }
 
-By Jonathan Deleon
+    greet('Jon', 'blue');
+    greet('Jane', 'purple');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
