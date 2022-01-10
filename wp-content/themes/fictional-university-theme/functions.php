c4bf47f3298d7ad1e5c816943d5b7f537c42e622
jtdeleon <jondeleon856@gmail.com>
2022-01-10 10:57:37 -0500

    update the styles for internal pages

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -5,9 +5,20 @@ function university_files() {
 
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    // Pull in external styles/support
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
 
 // Hook Name Matters Here
 add_action('wp_enqueue_scripts', 'university_files');
+
+
+function university_features() {
+    // There are other wp features to use too! 
+    // This support adds the title dynamically to the title tag of the page 
+    add_theme_support('title-tag');
+}
+
+// Hook Name Matters Here 
+add_action('after_setup_theme', 'university_features');
