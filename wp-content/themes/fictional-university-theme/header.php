a0997e1ea59944b6ea433f77431425eac3ca5900
jtdeleon <jondeleon856@gmail.com>
2022-01-10 17:34:52 -0500

    update the meta info

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html>
+<!-- Added a wordpress language attribute support for SEO/Visitors to know what language to expect -->
+<html <?php language_attributes() ?>>
     <head>
+        <!-- Gives website a charset of UTF-8 for nation charset used -->
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <!-- For mobile responsive -->
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
         <?php wp_head(); ?>
     </head>
-    <body>
+    <!-- Sets dynamic classes to the body tag depending on the page you are visiting. We can use the class names to style a particular page a certain way based on the page type in WP-->
+    <body <?php body_class(); ?>>
 
     <header class="site-header">
       <div class="container">
