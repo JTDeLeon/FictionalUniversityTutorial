15c25cf49c3b557b141e9b58e56ec7ebe0eae1ee
jtdeleon <jondeleon856@gmail.com>
2022-01-12 09:39:40 -0500

    build custom query for recent blog post home

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -49,21 +49,44 @@
           <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
+
+      <!-- Blog section -->
       <div class="full-width-split__two">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+          <?php  
+            // Custom Query Object
+            $homepagePosts = new WP_Query(array(
+              // Num of posts per query
+              'posts_per_page' => 2,
+              // Filter for cat
+              // 'category_name' => 'awards',
+              // Filter for type of post i.e. page
+              // 'post_type' => 'page'
+            ));
 
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Jan</span>
-              <span class="event-summary__day">20</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-              <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
+            // Access custom query functions
+            while($homepagePosts->have_posts()){
+              $homepagePosts->the_post();?>
+                <div class="event-summary">
+                  <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month"><?php the_time('M');?></span>
+                    <span class="event-summary__day"><?php the_time('d'); ?></span>
+                  </a>
+                  <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <!-- Limited word excerpts 1: Content you want to limit 2: words to limit to -->
+                    <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                  </div>
+                </div>
+              <?php
+            }
+            // IMPORTANT HABIT TO CLOSE and reset CUSTOM QUERY
+            wp_reset_postdata();
+          ?>
+
+          
+          <!-- <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
               <span class="event-summary__month">Feb</span>
               <span class="event-summary__day">04</span>
@@ -72,12 +95,14 @@
               <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
               <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
             </div>
-          </div>
+          </div> -->
 
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+          <p class="t-center no-margin"><a href="<?php echo site_url('blog') ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
       </div>
     </div>
+
+    <!-- End Blog Posts -->
 
     <div class="hero-slider">
       <div data-glide-el="track" class="glide__track">
